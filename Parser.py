fbe2197162b217036819664a3b81128cbbb97769
spoerlyannick@gmail.com <spoerlyannick@gmail.com>
2018-08-02 23:23:12 +0200

    now parsing all session data correctly

--- Parser.py
+++ Parser.py
@@ -4,36 +4,63 @@ from Session import Session
 
 
 class Parser:
+
+    # TODO: give path manually
     def __init__(self):
-        self.sport_sessions = []
+        self.sessions = []
         if os.path.exists("User") and os.path.isdir("User"):
             print("'User' directory found and added")
-            self.userDirexists = True
+            self.user_dir = "User/"
         else:
             print("'User' directory not found")
 
         if os.path.exists("Sport-sessions") and os.path.isdir("Sport-sessions"):
+            self.sport_sessions_dir = "Sport-sessions/"
             print("'Sport-sessions' directory found and added")
         else:
             print("'Sport-sessions' directory not found")
-            print("Please make sure the script lays in the same directory as the 'Sport-sessions directory, otherwise "
-                  "no data can be retrieved!")
 
     def initialize_sport_sessions(self):
-        files = os.listdir("Sport-sessions")
         sessions = []
-        for file in files:
-            if file[len(file)-5:len(file)] != ".json":
-                files.remove(file)
-        for file in files:
-            with open("Sport-sessions/" + file) as data:
-                sessions.append(json.load(data))
-        dates = []
+        new_sessions = []
+        for file in os.listdir(self.sport_sessions_dir):
+            if file[len(file)-5:len(file)] == ".json":
+                sessions.append(file)
         for session in sessions:
-            dates.append(datetime.fromtimestamp(session["start_time"]/1000+7200))
-        dates.sort()
-        for date in dates:
-            print(date)
+            with open(self.sport_sessions_dir + session) as data:
+                session_data = json.load(data)
+            try:
+                start_time = datetime.fromtimestamp((session_data["start_time"]+session_data["start_time_timezone_offset"])/1000)
+                end_time = datetime.fromtimestamp((session_data["end_time"]+session_data["end_time_timezone_offset"])/1000)
+                distance = session_data["distance"]/1000
+                duration = round(session_data["duration"]/1000)
+                elevation_gain = session_data["elevation_gain"]
+                elevation_loss = session_data["elevation_loss"]
+                average_speed = session_data["average_speed"]
+                calories = session_data["calories"]
+                longitude = session_data["longitude"]
+                latitude = session_data["latitude"]
+                max_speed = session_data["max_speed"]
+                pause_duration = round(session_data["pause_duration"]/1000)
+                pulse_avg = session_data["pulse_avg"]
+                pulse_max = session_data["pulse_max"]
+                avg_cadence = session_data["avg_cadence"]
+                max_cadence = session_data["max_cadence"]
+                manual = session_data["manual"]
+                edited = session_data["edited"]
+                completed = session_data["completed"]
+                indoor = session_data["indoor"]
+                id = session_data["id"]
+                sport_type_id = session_data["sport_type_id"]
+                new_session = Session(start_time, end_time, distance, duration, elevation_gain, elevation_loss,
+                                      average_speed, calories, longitude, latitude, max_speed, pause_duration,
+                                      pulse_avg, pulse_max, avg_cadence, max_cadence, manual, edited, completed, indoor,
+                                      id, sport_type_id)
+                new_sessions.append(new_session)
+                print("Processed session from " + start_time.strftime("%d.%m.%Y "))
+            except:
+                print("Couldnt process session " + session)
+        self.sessions = new_sessions
 
 
 p = Parser()
--- Session.py
+++ Session.py
@@ -1,25 +1,20 @@
 class Session:
-    def __init__(self, startTime, endTime, createdAt, updatedAt, startTimeTimezoneOffset, endTimeTimezoneOffset,
-                 distance, duration, elevationGain, elevationLoss, averageSpeed, calories, longitude, latitude,
-                 maxSpeed, pauseDuration, durationPerKM, pulseAVG, pulseMAX, avgCadence, maxCadence, manual, edited,
-                 completed, liveTrackingActive, liveTrackingEnabled, cheeringEnabled, indoor, id, sportTypeID):
-        self.startTime = startTime
-        self.endTime = endTime
-        self.createdAt = createdAt
-        self.updatedAt = updatedAt
-        self.startTimeTimezoneOffset = startTimeTimezoneOffset
-        self.endTimeTimezoneOffset = endTimeTimezoneOffset
-        self.distance = distance
-        self.duration = duration
-        self.elevationGain = elevationGain
-        self.elevationLoss = elevationLoss
-        self.averageSpeed = averageSpeed
+    def __init__(self, startTime, endTime,
+                 distance, duration, elevationGain, elevationLoss, averageSpeed, calories, longitude, latitude, maxSpeed,
+                 pauseDuration, pulseAVG, pulseMAX, avgCadence, maxCadence, manual, edited, completed, indoor, id,
+                 sportTypeID):
+        self.startTime = startTime  # in datetime
+        self.endTime = endTime      # in datetime
+        self.distance = distance  # in km
+        self.duration = duration  # in s
+        self.elevationGain = elevationGain  # in m
+        self.elevationLoss = elevationLoss  # in m
+        self.averageSpeed = averageSpeed  # in km/h
         self.calories = calories
         self.longitude = longitude
         self.latitude = latitude
-        self.maxSpeed = maxSpeed
-        self.pauseDuration = pauseDuration
-        self.durationPerKM = durationPerKM
+        self.maxSpeed = maxSpeed  # in km/h
+        self.pauseDuration = pauseDuration  # in s
         self.pulseAVG = pulseAVG
         self.pulseMAX = pulseMAX
         self.avgCadence = avgCadence
@@ -27,11 +22,11 @@ class Session:
         self.manual = manual
         self.edited = edited
         self.completed = completed
-        self.liveTrackingActive = liveTrackingActive
-        self.liveTrackingEnabled = liveTrackingEnabled
-        self.cheeringEnabled = cheeringEnabled
         self.indoor = indoor
         self.id = id
         self.sportTypeID = sportTypeID
         print("created new session")
+
+    def __str__(self):
+        return str(self.distance) + "km, " + str(round(self.duration/60)) + " min from " + self.startTime.strftime("%d.%m.%y")
 
